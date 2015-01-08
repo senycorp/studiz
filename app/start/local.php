1b0cf7b01653bde09ad064a1d8ca236ce4e13d26
Selcuk Kekec <skekec@synetics.de>
2015-01-08 19:36:39 +0100

    - FirePHP added as additional error handler to MonoLog

--- app/start/local.php
+++ app/start/local.php
@@ -1,3 +1,22 @@
 <?php
 
-//
+/*
+|--------------------------------------------------------------------------
+| Bootstrap for `local` environment
+|--------------------------------------------------------------------------
+|
+| Please place all environment specific bootstrap code here. So it
+| is separated from general stuff which is placed in app/start/global.php
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| FirePHP registration
+|--------------------------------------------------------------------------
+|
+| Register FirePHP as additional handler.
+|
+*/
+
+Log::getMonolog()->pushHandler(new \Monolog\Handler\FirePHPHandler());
