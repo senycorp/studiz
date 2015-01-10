d59fe904943d6dc302bcea5b58be7f0f169014be
Selcuk Kekec <senycorp@googlemail.com>
2015-01-10 22:32:24 +0100

    New exception types added.

---
+++ workbench/studiz/core/src/Studiz/Core/Exception/LogicalException.php
@@ -0,0 +1,11 @@
+<?php namespace Studiz\Core\Exception;
+
+/**
+ * Class LogicalException
+ *
+ * @author Selcuk Kekec <senycorp@googlemail.com>
+ * @package Studiz\Core\Exception
+ */
+class LogicalException extends CoreException {
+
+}
