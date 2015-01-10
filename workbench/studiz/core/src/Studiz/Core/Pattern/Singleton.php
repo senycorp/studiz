8eb942c14dff213ec7c0b75972e1ca2fce54e9d4
Selcuk Kekec <senycorp@googlemail.com>
2015-01-10 22:32:49 +0100

    Singleton pattern added

---
+++ workbench/studiz/core/src/Studiz/Core/Pattern/Singleton.php
@@ -0,0 +1,17 @@
+<?php namespace Studiz\Core\Pattern;
+
+/**
+ * Interface Singleton
+ *
+ * @author Selcuk Kekec <senycorp@googlemail.com>
+ * @package Studiz\Core\Pattern
+ */
+interface Singleton {
+
+    /**
+     * Get singleton instance
+     *
+     * @return static
+     */
+    public static function getInstance();
+}
