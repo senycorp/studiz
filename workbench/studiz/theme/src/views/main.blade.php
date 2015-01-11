ee66dc5d87fcbb983f599af36863cd45c48972e5
Selcuk Kekec <senycorp@googlemail.com>
2015-01-11 12:49:54 +0100

    studiz ********************************************* *   Issue #13                               * *********************************************
    Module: Theme
    
    Admin-lte transformed to blade templates with yields seperated in sections

---
+++ workbench/studiz/theme/src/views/main.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        @include('theme::head')
+    </head>
+    <body class="skin-blue">
+        @include('theme::top')
+        @include('theme::wrapper')
+    </body>
+</html>
