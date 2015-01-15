150a9d0f806589c2bf42da5aac95314962ac6814
Selcuk Kekec <senycorp@googlemail.com>
2015-01-15 18:31:39 +0100

    - Vendors ide helper and Sentry added

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'studiz',
 			'username'  => 'root',
-			'password'  => 'MKataturk89',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
