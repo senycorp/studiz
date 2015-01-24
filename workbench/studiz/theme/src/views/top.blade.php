e6a010070c0031dd88bc92ecfe884394b9403e68
Selcuk Kekec <senycorp@googlemail.com>
2015-01-24 16:01:51 +0100

    Cap added to logo section

--- workbench/studiz/theme/src/views/top.blade.php
+++ workbench/studiz/theme/src/views/top.blade.php
@@ -6,6 +6,7 @@
 <header class="header">
     <a href="index.html" class="logo">
         <!-- Add the class icon to your logo image or logo icon to add the margining -->
+        <i class="fa fa-graduation-cap"></i>
         Studiz
     </a>
     <!-- Header Navbar: style can be found in header.less -->
